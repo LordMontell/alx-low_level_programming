22be8cf760625eadd3b992def073805714b85a4e
Montell <shobax3@gmail.com>
2023-08-16 06:24:33 -0700

    program - print lowercase alphabet using a function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  * main - call a function
+  */
+
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+  * print_alphabet - print alphabet in lower case
+  */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c += 1;
+	}
+}
