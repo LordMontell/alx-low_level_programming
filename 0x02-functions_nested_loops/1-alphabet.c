2344a7268e81ad7ae57828487449f3d9d70fcc0a
Montell <shobax3@gmail.com>
2023-08-16 15:56:59 -0700

    program - print alphabets in lowercase using _printf function,fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,5 @@
 #include "main.h"
 /**
-  * main - call a print_alphabet() function
-  * Return: 0 when succesful
-  */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
   * print_alphabet - print alphabet in lower case
   */
 void print_alphabet(void)
