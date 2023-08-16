7f1931f736ba97934abbb851233674d8d678163c
Montell <shobax3@gmail.com>
2023-08-16 15:33:58 -0700

    program - prints 'putchar'

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
   * main - prints _putchar
   * Return: 0 when succesful
@@ -6,6 +6,13 @@
 
 int main(void)
 {
-	puts("_putchar");
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
