9082719157e0d3b85f331c184c4a8b58e4f58afc
Montell <shobax3@gmail.com>
2023-08-16 19:22:54 -0700

    program - add two integers together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * add - add two integers together
+  * @f: first integer argument
+  * @s: second integer argument
+  * Return: total of two integers
+  */
+
+int add(int f, int s)
+{
+	int total = 0;
+
+	total = f + s;
+	return (total);
+}
