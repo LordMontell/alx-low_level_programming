7597309b7279918ce7f47bb3cbbaa2dc346f07d4
Montell <shobax3@gmail.com>
2023-08-17 19:15:31 -0700

    program - fizzBuzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+  * main - fizzbuzz program
+  * Return: 0 when succesful
+  */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1; i < 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
