660273ef363b8037a1a31f205ab832ca73e1f442
Montell <shobax3@gmail.com>
2023-08-17 20:20:25 -0700

    fixed, main.h & 9-fizzbuzz

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef MY_MORE_FUNCTION_H
 #define MY_MORE_FUNCTION_H
-
+#include <stdio.h>
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,5 +11,4 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-int main(void);
 #endif
