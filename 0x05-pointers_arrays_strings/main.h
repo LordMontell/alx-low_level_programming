64673fd2fb58901e0b026e1e4e5e814710d3f3d2
Montell <shobax3@gmail.com>
2023-08-21 14:17:30 -0700

    updated main.h with _strlen(0 prototype

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef		My_Pointers_H
 #define		My_Pointers_H
-
+#include <string.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-
+int _strlen(char *s);
 #endif
