341d70a9b6fd312df026171502a54fe0d8483db3
Montell <shobax3@gmail.com>
2023-08-21 14:16:58 -0700

    program - counts length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * _strlen - determine the length of a string
+  * @s: Receives a string copy
+  * Return: the length of a string
+  */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
