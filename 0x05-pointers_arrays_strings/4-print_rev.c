60c656179d941518edc5f7d6e883819bb42a7d8f
Montell <shobax3@gmail.com>
2023-08-21 20:13:51 -0700

    program - prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_rev -print string in reverse
+  * @s: receives a copy of string
+  * Return: nothing
+  */
+
+void print_rev(char *s)
+{
+	int i;
+	int strl = (int) strlen(s);
+
+	for (i = strl; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
