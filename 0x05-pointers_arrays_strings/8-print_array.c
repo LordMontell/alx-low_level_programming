5abe556b0a7f69c92e674ecf25e1d17fd3d3f964
Montell <shobax3@gmail.com>
2023-08-21 19:01:41 -0700

    updated main.h with print_array() prototype

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * print_array - print array elements
+  * @a: receives a copy of an array
+  * @n: receives number of elements to count
+  * Return: nothing
+  */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d", *(a + i));
+		}
+		else
+		{
+			printf("%d; ", *(a + i));
+		}
+
+	}
+	printf("\n");
+}
