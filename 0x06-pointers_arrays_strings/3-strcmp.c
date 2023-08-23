82358262831b5fb6d26b71d5b3b341becbaf8ac6
Montell <shobax3@gmail.com>
2023-08-23 10:46:35 -0700

    program - compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * _strcmp - compares two string
+  * @s1: receives an adress of a first string
+  * @s2: receives an address of second string
+  * Return: an integer
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
