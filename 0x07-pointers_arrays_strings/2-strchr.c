2f1f713d0c2f07f96673801688a23a7f458a75e6
Montell <shobax3@gmail.com>
2023-08-29 16:43:15 -0700

    program - finds a character in a given string,fixed3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,5 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; i < (int)strlen(s); i++)
-	{
-		if (*(s + i) == c)
-		{
-			return (s + i);
-		}
-	}
-
-	return (NULL);
+	return (strchr(s, c));
 }
