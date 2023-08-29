21f243f875fbec4e17c5ab802b886382f7227c5b
Montell <shobax3@gmail.com>
2023-08-29 14:54:09 -0700

    program - fills a memory with constant to first n bytes

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * _memset - function fills the first n bytes of the memory
+  *		area pointed to by s with the constant byte b
+  * @s: receives address to be filled
+  * @b: receives constant byte to be filled in the memmory
+  * @n: number of bytes to be filled
+  * Return: a pointer of the filled address are
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*s = b;
+		s++;
+	}
+	return (s);
+
+}
