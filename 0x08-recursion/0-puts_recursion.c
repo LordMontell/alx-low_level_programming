a626100b491770a7f7a69e613a7b2c2f15cb2655
Montell <shobax3@gmail.com>
2023-08-31 17:39:07 -0700

    program - prints out a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * _puts_recursion - print out a string using recursion
+  * @s: Receives string address
+  * Return: nothing
+  */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
